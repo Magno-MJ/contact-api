5cda113042d04ad4215680257ce5b977da94f7ea
Magno Jovino <magno.jovino@hotmail.com>
2024-08-22 17:48:26 -0300

    feat: add data handler

---
+++ src/main/java/com/study/contactapi/utils/data/DataHandler.java
@@ -0,0 +1,27 @@
+package com.study.contactapi.utils.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+
+@Service
+public class DataHandler {
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  /* https://stackoverflow.com/questions/38424383/how-to-distinguish-between-null-and-not-provided-values-for-partial-updates-in-s */
+  public <T> T  mergeData(Object source, Object target) {
+    ObjectReader readerForUpdating = objectMapper.readerForUpdating(source);
+    
+    try {
+      T merged = readerForUpdating.readValue(objectMapper.convertValue(target, JsonNode.class));
+
+      return merged;
+    } catch (Exception exception) {
+      return null;
+    }
+  }
+}
