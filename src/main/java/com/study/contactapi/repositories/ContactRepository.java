49a1c014f3858ea30ba2d7dc1d74bd45712f44de
Magno Jovino <magno.jovino@hotmail.com>
2024-08-22 17:46:33 -0300

    feat: add contact repository

---
+++ src/main/java/com/study/contactapi/repositories/ContactRepository.java
@@ -0,0 +1,24 @@
+package com.study.contactapi.repositories;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.study.contactapi.domain.user.Contact;
+
+import jakarta.transaction.Transactional;
+
+public interface ContactRepository extends JpaRepository<Contact, String>{
+  @Query(value = "SELECT * FROM contact WHERE id = ?1 AND contact_user_id = ?2", nativeQuery = true)
+  Optional<Contact> findByIdAndUserId(String contactId, String userId);
+
+  @Query(value = "SELECT * FROM contact WHERE contact_user_id = ?1", nativeQuery = true)
+  List<Contact> findAllByContactUserId(String userId);
+
+  @Transactional
+  @Query(value = "DELETE FROM contact WHERE id = ?1 AND contact_user_id = ?2", nativeQuery = true)
+  Optional<Contact> deleteByIdAndUserId(String contactId, String userId);
+}
