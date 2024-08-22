a848fafd55e1276268960587866af1c30898c219
Magno Jovino <magno.jovino@hotmail.com>
2024-08-22 17:46:46 -0300

    feat: add user repository

---
+++ src/main/java/com/study/contactapi/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.study.contactapi.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.study.contactapi.domain.user.User;
+
+public interface UserRepository extends JpaRepository<User, String>{
+}
