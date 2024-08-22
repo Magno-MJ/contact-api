fb0b7f0befbfd481c08422d2a2ed45b00fd51185
Magno Jovino <magno.jovino@hotmail.com>
2024-08-22 17:46:26 -0300

    feat: add account confirmation token repository

---
+++ src/main/java/com/study/contactapi/repositories/AccountConfirmationTokenRepository.java
@@ -0,0 +1,24 @@
+package com.study.contactapi.repositories;
+
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.study.contactapi.domain.user.AccountConfirmationToken;
+
+
+public interface AccountConfirmationTokenRepository extends JpaRepository<AccountConfirmationToken, String> {
+    AccountConfirmationToken findByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE account_confirmation_token SET is_active = false WHERE token = ?1 AND is_active = true", nativeQuery = true)
+    void disableByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE account_confirmation_token SET is_active = false WHERE login_id = ?1 AND is_active = true", nativeQuery = true)
+    void disableByLoginId(String loginId);
+}
