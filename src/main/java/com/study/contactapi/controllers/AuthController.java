72f2180f68cdb8c676401438aa5be6e71f8cb8eb
Magno Jovino <magno.jovino@hotmail.com>
2024-08-22 17:45:21 -0300

    feat: add auth controller

---
+++ src/main/java/com/study/contactapi/controllers/AuthController.java
@@ -0,0 +1,40 @@
+package com.study.contactapi.controllers;
+
+import com.study.contactapi.dto.LoginBodyDTO;
+import com.study.contactapi.dto.LoginResponseDTO;
+import com.study.contactapi.dto.ResendAccountConfirmationTokenBodyDto;
+import com.study.contactapi.services.AuthService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+  private final AuthService authService;
+
+  @PostMapping("/login")
+  public ResponseEntity<LoginResponseDTO> login(@Validated @RequestBody LoginBodyDTO loginBodyDto){
+    ResponseEntity<LoginResponseDTO> loginResponseDto = this.authService.login(loginBodyDto);
+
+    return loginResponseDto;
+  }
+
+  @PostMapping("/confirm/{accountConfirmationToken}")
+  public void confirmAccount(@PathVariable String accountConfirmationToken) {
+    this.authService.confirmAccount(accountConfirmationToken);
+  }
+
+  @PostMapping("/resend-confirmation-token")
+  public void resendAccountConfirmationToken(@Validated @RequestBody ResendAccountConfirmationTokenBodyDto resendAccountConfirmationTokenBodyDto) {
+    this.authService.resendAccountConfirmationToken(resendAccountConfirmationTokenBodyDto);
+  }
+}
