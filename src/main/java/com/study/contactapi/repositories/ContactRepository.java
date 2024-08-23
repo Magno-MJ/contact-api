7b4d54fff1d42bab30d00f5b392f539c4693f66c
Magno Jovino <magno.jovino@hotmail.com>
2024-08-23 13:48:30 -0300

    refactor: remove unused query

--- src/main/java/com/study/contactapi/repositories/ContactRepository.java
+++ src/main/java/com/study/contactapi/repositories/ContactRepository.java
@@ -9,16 +9,10 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.study.contactapi.domain.user.Contact;
 
-import jakarta.transaction.Transactional;
-
 public interface ContactRepository extends JpaRepository<Contact, String>{
   @Query(value = "SELECT * FROM contact WHERE id = ?1 AND contact_user_id = ?2", nativeQuery = true)
   Optional<Contact> findByIdAndUserId(String contactId, String userId);
 
   @Query(value = "SELECT * FROM contact WHERE contact_user_id = ?1", nativeQuery = true)
   List<Contact> findAllByContactUserId(String userId);
-
-  @Transactional
-  @Query(value = "DELETE FROM contact WHERE id = ?1 AND contact_user_id = ?2", nativeQuery = true)
-  Optional<Contact> deleteByIdAndUserId(String contactId, String userId);
 }
