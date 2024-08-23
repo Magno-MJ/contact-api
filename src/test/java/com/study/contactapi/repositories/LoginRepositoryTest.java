f82871460ac136b39c6c786dda95d2ce774eed4c
Magno Jovino <magno.jovino@hotmail.com>
2024-08-23 13:49:58 -0300

    feat: add login repository tests

---
+++ src/test/java/com/study/contactapi/repositories/LoginRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.study.contactapi.repositories;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.study.contactapi.domain.user.Login;
+
+import jakarta.persistence.EntityManager;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class LoginRepositoryTest {
+  
+  @Autowired
+  EntityManager entityManager;
+
+  @Autowired
+  LoginRepository loginRepository;
+
+  @Test
+  @DisplayName("Should activate user login by id successfully")
+  void activateUserLoginById() {
+    Login login = this.createLogin(new Login("fake@mail.com", "fake-password"));
+
+    this.loginRepository.activateUserLoginById(login.getId());
+
+    Login activatedLogin =  this.loginRepository.findById(login.getId()).get();
+
+    System.out.println(activatedLogin);
+    assertThat(activatedLogin.getAccount_activated_at()).isToday();
+  }
+
+
+  private Login createLogin(Login login) {
+    this.entityManager.persist(login);
+    return login;
+  }
+}
