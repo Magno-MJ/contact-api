8263b737e1b1cb85ce85b1dddf456cfb66d89061
Magno Jovino <magno.jovino@hotmail.com>
2024-08-25 16:56:22 -0300

    feat: add server details in documentation

--- src/main/java/com/study/contactapi/docs/SwaggerConfig.java
+++ src/main/java/com/study/contactapi/docs/SwaggerConfig.java
@@ -6,11 +6,23 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 	@Bean
 	public OpenAPI customOpenAPI() {
-		return new OpenAPI().info(new Info().title("Contact API Docs"));
+    Server server = new Server();
+
+    server.setUrl("http://localhost:8080");
+    server.description("Contact API server");
+
+		return new OpenAPI().info(
+      new Info()
+        .title("Contact API Docs")
+        .description("Contact API routes documentation")
+        .version("1.0"))
+        .servers(List.of(server));
 	}
 }
