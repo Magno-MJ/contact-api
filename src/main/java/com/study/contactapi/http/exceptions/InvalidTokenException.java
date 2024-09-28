2f363ac4a04433f958a4cd7422b06b915aff47d0
Magno Jovino <magno.jovino@hotmail.com>
2024-09-28 19:19:25 -0300

    refactor: format InvalidTokenException code

--- src/main/java/com/study/contactapi/http/exceptions/InvalidTokenException.java
+++ src/main/java/com/study/contactapi/http/exceptions/InvalidTokenException.java
@@ -3,7 +3,7 @@ package com.study.contactapi.http.exceptions;
 import com.study.contactapi.http.exceptions.general.ForbiddenException;
 
 public class InvalidTokenException extends ForbiddenException {
-  public InvalidTokenException() {
-    super("Invalid token");
-  }
+    public InvalidTokenException() {
+        super("Invalid token");
+    }
 }
