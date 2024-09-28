cdb50d5755684309d77e932243e12702d69a709b
Magno Jovino <magno.jovino@hotmail.com>
2024-09-28 19:20:01 -0300

    refactor: format application code

--- src/main/java/com/study/contactapi/ContactApiApplication.java
+++ src/main/java/com/study/contactapi/ContactApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContactApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContactApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ContactApiApplication.class, args);
+    }
 }
