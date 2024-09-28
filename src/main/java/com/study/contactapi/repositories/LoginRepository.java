9bf225573936e7017d360bf5d0e6d12de9bb4371
Magno Jovino <magno.jovino@hotmail.com>
2024-09-28 19:18:50 -0300

    refactor: format repositories code

--- src/main/java/com/study/contactapi/repositories/LoginRepository.java
+++ src/main/java/com/study/contactapi/repositories/LoginRepository.java
@@ -10,11 +10,11 @@ import com.study.contactapi.domain.user.Login;
 
 import jakarta.transaction.Transactional;
 
-public interface LoginRepository extends JpaRepository<Login, String>{
-  Optional<Login> findByEmail(String email);
-  
-  @Transactional  
-  @Modifying(clearAutomatically = true)
-  @Query(value = "UPDATE login SET account_activated_at = NOW() WHERE id = ?1", nativeQuery = true)
-  void activateUserLoginById(String id);
+public interface LoginRepository extends JpaRepository<Login, String> {
+    Optional<Login> findByEmail(String email);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE login SET account_activated_at = NOW() WHERE id = ?1", nativeQuery = true)
+    void activateUserLoginById(String id);
 }
