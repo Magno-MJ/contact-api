ed4a5851925f5fbc752b598f9f5e8bc594bef946
Magno Jovino <magno.jovino@hotmail.com>
2024-09-28 19:36:00 -0300

    fix: change update contact dto validation

--- src/main/java/com/study/contactapi/dto/UpdateContactBodyDTO.java
+++ src/main/java/com/study/contactapi/dto/UpdateContactBodyDTO.java
@@ -17,15 +17,13 @@ import lombok.Setter;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class UpdateContactBodyDTO {
     @Schema(description = "Contact first name", example = "John")
-//    @NotBlank(message = "first_name is required")
     String first_name;
 
     @Schema(description = "Contact last name", example = "Doe")
-//    @NotBlank(message = "last_name is required")
     String last_name;
 
     @Schema(description = "Contact phone number", example = "99999999999")
-//    @NotBlank(message = "phone_number is required")
+
     @Size(min = 11, max = 11, message = "phone_number is invalid")
     String phone_number;
 }
