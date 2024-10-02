c8c70b1882dcbec14f6996425e2c2b0e854519ed
Magno Amaral <magno.amaral@hvex.com.br>
2024-10-01 22:40:08 -0300

    refactor: change imports

--- src/test/java/com/study/contactapi/repositories/AccountConfirmationTokenRepositoryTest.java
+++ src/test/java/com/study/contactapi/repositories/AccountConfirmationTokenRepositoryTest.java
@@ -2,8 +2,7 @@ package com.study.contactapi.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
